811e04c8f4d65f133eff2d8ebd4593a31b9f8448
Vlad Deynega <36485560+vladdeynega@users.noreply.github.com>
2020-07-20 00:59:07 +0700

    Пауза
    Добавлена пауза

--- Assets/Scripts/CanvasButtons.cs
+++ Assets/Scripts/CanvasButtons.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CanvasButtons : MonoBehaviour
+public class CanvasButtons : MonoBehaviour,IPointerDownHandler
 {
     /*Тут работа с кенвасом*/
 
     public bool StartStatus = false;  // проверка нажатия кнопки старт
-    public void StartGame()
+    public new GameObject camera;
+    public GameObject canvas;
+    bool isPaused = false;
+    
+    public void OnPointerDown(PointerEventData eventData)
+
     {
-        Camera.main.transform.position = new Vector3(5f, 7f, 0); // плохая попытка смены положения камеры :)
-        StartStatus = true;// 
+        if (isPaused == true)
+        {
+            Time.timeScale = 1;
+            canvas.GetComponent<Canvas>().enabled = false;
+            isPaused = false;
+        }
+        camera.transform.position = new Vector3(0, 8.83f, -5.83f);
+        camera.GetComponent<Camera>().fieldOfView = 42.46f;
+        canvas.GetComponent<Canvas>().enabled = false;
+        
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            print("pause");
+            if (isPaused == false)
+            {
+                Debug.Log("pause");
+                Time.timeScale = 0;
+                canvas.GetComponent<Canvas>().enabled = true;
+                isPaused = true;
+
+            }
+            else
+            {
+                Debug.Log("unpause");
+                Time.timeScale = 1;
+                canvas.GetComponent<Canvas>().enabled = false;
+                isPaused = false;
+
+            }
+        }
+    }
 }
